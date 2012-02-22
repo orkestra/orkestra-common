5378a6a37d893a55a72e5af5d8dd6d47481654d3
Tyler Sommer <sommertm@gmail.com>
2012-02-21 17:32:44 -0700

    Added Entity base class and LifecycleEventSubscriber for hooking into the lifecycle of entities. fixes #502 #618

---
+++ lib/Orkestra/Common/Entity/EntityBase.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace Orkestra\Common\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+use Orkestra\Common\Type\DateTime,
+    Orkestra\Common\Type\NullDateTime;
+
+/**
+ * Entity Base
+ *
+ * Base class for all entities
+ *
+ * @ORM\MappedSuperClass
+ * @ORM\HasLifecycleCallbacks
+ */
+abstract class EntityBase implements IEntity
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    protected $id;
+
+    /**
+     * @var boolean $active
+     *
+     * @ORM\Column(name="active", type="boolean")
+     */
+    protected $active = true;
+
+    /**
+     * @var Orkestra\Common\Type\DateTime $dateModified
+     *
+     * @ORM\Column(name="date_modified", type="datetime", nullable=true)
+     */
+    protected $dateModified;
+
+    /**
+     * @var Orkestra\Common\Type\DateTime $dateCreated
+     *
+     * @ORM\Column(name="date_created", type="datetime")
+     */
+    protected $dateCreated;
+    
+    /**
+     * @var array An array of bound event listeners
+     */
+    protected $_listeners = array();
+    
+    /**
+     * To String
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return sprintf('%s:%s', get_class($this), spl_object_hash($this));
+    }
+        
+    /**
+     * Get ID
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+	/**
+     * Set Active
+     *
+     * @param boolean
+     */
+    public function setActive($active)
+    {
+        $this->active = $active ? true : false;
+    }
+
+    /**
+     * Get Active
+     *
+     * @return boolean
+     */
+    public function getActive()
+    {
+        return $this->active;
+    }
+    
+    /**
+     * Is Active
+     *
+     * @return boolean
+     */
+    public function isActive()
+    {
+        return $this->getActive();
+    }
+
+    /**
+     * Get Date Created
+     *
+     * @return Orkestra\Common\Type\DateTime
+     */
+    public function getDateCreated()
+    {
+        if (empty($this->dateCreated)) {
+            $this->dateCreated = new NullDateTime();
+        }
+        
+        return $this->dateCreated;
+    }
+
+    /**
+     * Get Date Modified
+     *
+     * @return Orkestra\Common\Type\DateTime
+     */
+    public function getDateModified()
+    {
+        if (empty($this->dateModified)) {
+            $this->dateModified = new NullDateTime();
+        }
+        
+        return $this->dateModified;
+    }
+
+    /**
+     * @ORM\prePersist
+     */
+    public function prePersist()
+    {
+        $this->dateCreated = new DateTime();
+    }
+
+    /**
+     * @ORM\preUpdate
+     */
+    public function preUpdate()
+    {
+        $this->dateModified = new DateTime();
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @ORM\prePersist
+	 * @ORM\preUpdate
+     */
+    public function validate()
+    {
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function addListener($event, $callback)
+    {
+        $this->_listeners[$event][] = $callback;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function getListeners($event)
+    {
+        return empty($this->_listeners[$event]) ? array() : $this->_listeners[$event];
+    }
+}
---
+++ lib/Orkestra/Common/Entity/IEntity.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Orkestra\Common\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Entity Interface
+ *
+ * Defines the contract any entity must follow
+ */
+interface IEntity { 
+    /**
+     * Validate
+     *
+     * Validates the entity, throwing an exception if any invalid fields are found
+     */
+    public function validate();
+    
+    /**
+     * Add Listener
+     *
+     * Registers a callback to listen for an event to fire
+     *
+     * @param string $event The name of the event
+     * @param callback $callback A valid callback
+     */
+    public function addListener($event, $callback);
+    
+    /**
+     * Get Listeners
+     *
+     * Gets an array of listeners for a specified event
+     *
+     * @param string $event The name of the event
+     * @return array An array of listeners
+     */
+    public function getListeners($event);
+}
