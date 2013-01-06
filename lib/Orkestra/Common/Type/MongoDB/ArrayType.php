0400c68efe7fb66b053a62336119cf0de49c4159
Zach Badgett <zach.badgett@gmail.com>
2013-01-05 19:01:03 -0700

    Added ArrayType for MongoDB ODM

---
+++ lib/Orkestra/Common/Type/MongoDB/ArrayType.php
@@ -0,0 +1,37 @@
+<?php
+namespace Orkestra\Common\Type\MongoDB;
+
+use Doctrine\ODM\MongoDB\Mapping\Types\Type;
+use Orkestra\Common\Exception\TypeException;
+
+class ArrayType extends Type
+{
+    public function convertToDatabaseValue($value)
+    {
+        return serialize($value);
+    }
+
+    public function convertToPHPValue($value)
+    {
+        if ($value === null) {
+            return null;
+        }
+
+        $value = (is_resource($value)) ? stream_get_contents($value) : $value;
+        $val = unserialize($value);
+        if ($val === false && $value != 'b:0;') {
+            throw new TypeException('Invalid array conversion');
+        }
+        return $val;
+    }
+
+    public function closureToMongo()
+    {
+        return '$return = $value;';
+    }
+
+    public function closureToPHP()
+    {
+        return '$return = $value;';
+    }
+}
